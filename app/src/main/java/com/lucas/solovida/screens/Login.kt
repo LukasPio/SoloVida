31d51726fcda04a62928d98e3f5794c68cb3cfbb
LucasPio <lucaspio.galvao@gmail.com>
2025-12-15 00:20:18 -0300

    create login screen

--- app/src/main/java/com/lucas/solovida/screens/Login.kt
+++ app/src/main/java/com/lucas/solovida/screens/Login.kt
@@ -1,7 +1,5 @@
 package com.lucas.solovida.screens
 
-import android.graphics.drawable.shapes.OvalShape
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -11,15 +9,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,19 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.ShaderBrush
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.lucas.solovida.R
 import com.lucas.solovida.components.AppLogo
 import com.lucas.solovida.components.EmailInputField
 import com.lucas.solovida.components.PasswordInputField
-import com.lucas.solovida.ui.theme.Brown
 
 @Composable
 fun Login(navController: NavController) {
